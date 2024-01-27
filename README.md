c3d429b961067e539f1e6cab60fbdb0622465194
Vitorg96 <138641243+Vitorg96@users.noreply.github.com>
2024-01-27 14:37:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">Whaticket Baileys</h1>
+<div align="center">
+<h1 align="center">https://www.whamax.com.br</h1>
---
+++ utils/_banner.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+  
+printf "${red}██╗    ██╗██╗  ██╗ █████╗ ███╗   ███╗ █████╗ ██╗  ██╗{reset}\n"
+printf "${red}██║    ██║██║  ██║██╔══██╗████╗ ████║██╔══██╗╚██╗██╔╝{reset}\n"
+printf "${red}██║ █╗ ██║███████║███████║██╔████╔██║███████║ ╚███╔╝ {reset}\n"
+printf "${red}██║███╗██║██╔══██║██╔══██║██║╚██╔╝██║██╔══██║ ██╔██╗ {reset}\n"
+printf "${red}╚███╔███╔╝██║  ██║██║  ██║██║ ╚═╝ ██║██║  ██║██╔╝ ██╗{reset}\n"
+
+  printf "            \033[1;33m        © WHAMAX - https://www.whamax.com.br";
+  printf "${NC}";
+
+  printf "\n"
+}
